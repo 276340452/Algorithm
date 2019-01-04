9a761153a2542373ccb821ff070ed785518898b2
zhiping.li <zhiping.li@qq.com>
2019-01-04 16:49:26 +0800

    feat:添加冒泡排序选择排序插入排序和希尔排序算法

--- ChapterOne/1-1.py
+++ ChapterOne/1-1.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 import random
 import time
+import math
 
 def main():
   N = eval(input('请输入N（N为正整数）：'))
@@ -11,11 +12,65 @@ def main():
     arr.append(int(random.uniform(1,50000)))
     i = i + 1
   start = time.process_time()
-  BubbleSort(arr)
+  # BubbleSort(arr)
+  # SelectionSort(arr)
+  # InsertionSort(arr)
+  ShellSort(arr)
   end = time.process_time()
   print(arr)
   k = int(N/2)
   print('当N=%d时排序花费时间为：%s,k=%s' % (N,end - start,arr[k]))
+
+def ShellSort(arr):
+  gap = 1
+  while gap < len(arr):
+    gap = gap * 3 + 1
+  
+  while gap > 0:
+    i = gap
+    while i < len(arr):
+      current = arr[i]
+      prePos = i - gap
+      while prePos >= 0 and arr[prePos] > current:
+        arr[prePos + gap] = arr[prePos]
+        prePos = prePos - gap
+      arr[prePos + gap] = current
+      i = i + 1
+    gap = math.floor(gap/3)
+
+def InsertionSort(arr):
+  i = 0
+  while i < len(arr):
+    current = arr[i]
+    prePos = i - 1
+    while prePos >= 0 and arr[prePos] > current:
+      arr[prePos + 1] = arr[prePos]
+      prePos = prePos - 1
+    arr[prePos + 1] = current
+    i = i + 1
+
+def SelectionSort(arr):
+  i = 0
+  left = 0
+  right = len(arr)-1
+  while left < right:
+    minPos = left
+    maxPos = right
+    i = left
+    while i <= right:
+      if arr[i] > arr[maxPos]:
+        maxPos = i
+      if arr[i] < arr[minPos]:
+        minPos = i
+      i = i + 1
+    temp = arr[left]
+    arr[left] = arr[minPos]
+    arr[minPos] = temp
+    temp = arr[right]
+    arr[right] = arr[maxPos]
+    arr[maxPos] = temp
+    left = left + 1
+    right = right - 1
 
 def BubbleSort(arr):
   i = 0
